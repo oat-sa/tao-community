27651ad2fe092c3c4ac43bb6aeff6709f9dc5e75
Aleh Hutnikau <hutnikau@1pt.com>
2021-03-31 18:58:14 +0300

    chore: delete redundant files

--- generateComposer.php
+++
@@ -1,7 +0,0 @@
-<?php
-$composerArray = json_decode(file_get_contents("./composer.json"), true);
-foreach ($composerArray["require"] as &$repository) {
-    $repository = '*';
-}
-file_put_contents("./composer-release.json", json_encode($composerArray, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES));
-
