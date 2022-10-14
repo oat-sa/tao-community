ff717cce6c7fc80f77e1fd7c4ffb6810b827bc74
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2022-10-14 17:52:07 +0200

    ci: add detailed summary

---
+++ .github/parse-reports.php
@@ -0,0 +1,11 @@
+<?php
+
+$pattern = '/testFailed\s+name=\'(?<name>[^\']+)\'\s+message=\'(?<message>.*)\'\s+details=\'.*tao-community\/(?<details>.*)\|n/m';
+
+preg_match_all($pattern, file_get_contents('/tmp/failures.txt'), $matches, PREG_SET_ORDER);
+
+$i = 0;
+foreach ($matches as $match) {
+    $i++;
+    echo "$i. **{$match['details']}**\n{$match['name']} {$match['message']}\n\n";
+}
