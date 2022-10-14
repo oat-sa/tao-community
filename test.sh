c2eb51eb861a7c52da23df1ef159ce0f90bb92c8
Sergei Mikhailov <sergei.mikhailov@taotesting.com>
2022-10-14 16:29:39 +0200

    ci(debug): job summary

--- test.sh
+++ test.sh
@@ -42,5 +42,5 @@ for d in */; do
   cp "$DIR/phpunit.xml.dist" "$DIR/phpunit.xml"
   sed -ie "s/\*/$d/g" "$DIR/phpunit.xml"
 
-  php $coverage "$DIR/vendor/bin/phpunit" --coverage-php "coverage/$d.php"
+  php -derror_reporting=24575 $coverage "$DIR/vendor/bin/phpunit" --coverage-php "coverage/$d.php"
 done
